669366dcd85fd3a0985a8de89599faa72a8f0404
Maaike Ongena <maaike.ongena@asadventure.com>
2015-03-10 16:05:24 +0100

    added back-end

---
+++ foldersysteem/application/models/products_model.php
@@ -0,0 +1,24 @@
+<?php
+class Products_model extends CI_Model {
+
+	public function __construct()
+	{
+		$this->load->database();
+	}
+
+	public function get_products($id = FALSE)
+	{
+		if ($id === FALSE)
+		{
+			$this->db->select('Products.* , Merken.*');
+			$this->db->from('Products');
+			$this->db->join('Merken', 'Products.merk = Merken.merknaam','left');
+			$query = $this->db->get();
+			return $query->result_array();
+		}
+
+		$query = $this->db->get_where('Products', array('id' => $id));
+		return $query->row_array();
+	}
+
+}
