a5735dbcea682fc168acd7eaad55ff2c77a587ce
Maaike Ongena <maaike.ongena@asadventure.com>
2015-03-24 09:48:08 +0100

    get links from database

--- foldersysteem/application/views/folder.php
+++ foldersysteem/application/views/folder.php
@@ -11,6 +11,7 @@
 </head>
 <body>
 	<div id="lp-folder-wrapper">
+		<?php var_dump($links) ?>
 		<!-- Header -->
 		<div class="video-wrapper">
 			<iframe src="https://player.vimeo.com/video/122988356" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
