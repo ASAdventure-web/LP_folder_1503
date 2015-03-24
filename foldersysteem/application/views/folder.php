23009c5546c7c693bc2a6f33803dac2751180019
Maaike Ongena <maaike.ongena@asadventure.com>
2015-03-24 10:30:16 +0100

    added comment on folder.php in foldersysteem

--- foldersysteem/application/views/folder.php
+++ foldersysteem/application/views/folder.php
@@ -15,6 +15,7 @@
 		<div class="video-wrapper">
 			<iframe src="https://player.vimeo.com/video/122988356" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 		</div>
+		<!-- filermenu -->
 		<ul>
 		<?php foreach ($links as $link) { ?>
 			<li>
